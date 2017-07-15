60c260c291fb16469ccd018672dbadaa1b6eac40
Psychoticvenom <x231231x@gmail.com>
2017-07-15 11:43:18 +0800

    FSMS

--- HunterFSM.js
+++ HunterFSM.js
@@ -1,4 +1,4 @@
-var hunterFSM = function (){
+var hunterFSM = function (mesh, text){
   
   var timer;
   
@@ -19,86 +19,94 @@ var hunterFSM = function (){
 	],
 
     callbacks: {
-      onbeforestart:      function(event, from, to) { },
-      onstart:            function(event, from, to) { },
+		onbeforestart:      function(event, from, to) { },
+		onstart:            function(event, from, to) { },
 
-	  onbeforefound:      function(event, from, to) { },
-      onbeforehear:       function(event, from, to) { },
-	  onbeforestanded:    function(event, from, to) { },
-	  onbeforepatroled:   function(event, from, to) { },
-	  onbeforelost:       function(event, from, to) { },
-	  onbeforenfound:     function(event, from, to) { },
-      onbeforecatched:    function(event, from, to) { },
+		onbeforefound:      function(event, from, to) { },
+		onbeforehear:       function(event, from, to) { },
+		onbeforestanded:    function(event, from, to) { },
+		onbeforepatroled:   function(event, from, to) { },
+		onbeforelost:       function(event, from, to) { },
+		onbeforenfound:     function(event, from, to) { },
+		onbeforecatched:    function(event, from, to) { },
 
-      onfound:            function(event, from, to) { 
-		//視線捕捉到玩家
-	  },
-      onhear:             function(event, from, to) {
-		//聽力範圍接觸到玩家
-	  },
-	  onstanded:          function(event, from, to) {
-		//經過一段時間後，開始巡邏
-	  },
-	  onpatroled:         function(event, from, to) {
-		//巡邏一段時間後，回到站立狀態
-	  },
-	  onlost:             function(event, from, to) {
-		//完全追丟玩家
-	  },
-	  onnfound:           function(event, from, to) {
-		//玩家消失在視線中
-	  },
-	  oncatched:          function(event, from, to) {
-		//沒有聽到玩家的動靜
-	  },
+		onfound:            function(event, from, to) { 
+			//視線捕捉到玩家
+		},
+		onhear:             function(event, from, to) {
+			//聽力範圍接觸到玩家
+		},
+		onstanded:          function(event, from, to) {
+			//經過一段時間後，開始巡邏
+		},
+		onpatroled:         function(event, from, to) {
+			//巡邏一段時間後，回到站立狀態
+		},
+		onlost:             function(event, from, to) {
+			//完全追丟玩家
+		},
+		onnfound:           function(event, from, to) {
+			//玩家消失在視線中
+		},
+		oncatched:          function(event, from, to) {
+			//沒有聽到玩家的動靜
+		},
 
-      onleavestand:       function(event, from, to) { },
-      onleavesearch:      function(event, from, to) { },
-	  onleaveseek:        function(event, from, to) { },
-      onleavepatrol:      function(event, from, to) { },
+		onleavestand:       function(event, from, to) { },
+		onleavesearch:      function(event, from, to) { },
+		onleaveseek:        function(event, from, to) { },
+		onleavepatrol:      function(event, from, to) { },
 
-	  onstand:            function(event, from, to) {
+		onstand:            function(event, from, to) {
 		
-		//站定點（放慢速）觀察
-		//每兩秒轉換成patrol狀態
-		clearTimeout(timer);
-		timer = setTimeout (function() {
-			fsm.standed();
-  		},2000);
+			//站定點（放慢速）觀察
+			//每兩秒轉換成patrol狀態
+			text.text = "Hunter : Stand";
+			mesh.material = new THREE.MeshLambertMaterial({color: 0xffffff});
 		
-	  },
-      onsearch:           function(event, from, to) {
+			clearTimeout(timer);
+			timer = setTimeout (function() {
+				fsm.standed();
+			},2000);
+		},
+		onsearch:           function(event, from, to) {
 		
-		//原地旋轉尋找玩家
-		clearTimeout(timer);
+			//原地旋轉尋找玩家
+			text.text = "Hunter : Search";
+			mesh.material = new THREE.MeshLambertMaterial({color: 0xffff00}); 
 		
-	  },
-      onseek:             function(event, from, to) {
+			clearTimeout(timer);
+		},
+		onseek:             function(event, from, to) {
 		
-		//追逐玩家
-		clearTimeout(timer);
+			//追逐玩家
+			text.text = "Hunter : Seek" ;
+			mesh.material = new THREE.MeshLambertMaterial({color: 0xff0000});
 		
-	  },
-	  onpatrol:           function(event, from, to) {
+			clearTimeout(timer);
+		},
+		onpatrol:           function(event, from, to) {
 		
-		//隨機給予一個target使獵人往那方向行走
-		//每五秒回到stand狀態
-		clearTimeout(timer);
-		timer = setTimeout (function() {
-			fsm.patroled();
-  		},5000);
+			//隨機給予一個target使獵人往那方向行走
+			//每五秒回到stand狀態
+			text.text = "Hunter : Patrol" ;
+			mesh.material = new THREE.MeshLambertMaterial({color: 0x000000});
 		
-	  },
-      oncatch:            function(event, from, to) { 
+			clearTimeout(timer);
+			timer = setTimeout (function() {
+				fsm.patroled();
+			},5000);
+		},
+		oncatch:            function(event, from, to) { 
 		
-		//遊戲結束
-		clearTimeout(timer);
+			//遊戲結束
+			text.text = "Hunter : Catch";
 		
-	  }
+			clearTimeout(timer);
+		}
     }
   });
 
   fsm.start();
   return fsm;
-
 }
